201327e2a4d6e98f91b406b369ad39c17ee928f9
PallaviH-S <palvisuresh56@gmail.com>
2026-05-08 19:52:51 +0530

    change

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password=$_POST['password'];
-
-if($username == "admin"&& $password == "1234"){
-    header("Location:main.html");
-}else{
-    echo "Invalid Login";
-}
